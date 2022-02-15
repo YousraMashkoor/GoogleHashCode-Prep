5acb4358ce5ee58347287b8f71c75ceae117fd30
YousraMashkoor <yousra.mashkoor@bboxx.co.uk>
2022-02-15 20:24:16 +0500

    testing all test cases

--- PizzaProblem/sol.py
+++ PizzaProblem/sol.py
@@ -1,44 +1,46 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 class OnePizza:
 
-    def __init__(self):
+    def __init__(self, file):
+        self.cus = None
         self.likes = Counter()
         self.dislikes = Counter()
-        self.c = None
-
+        self.filename = file
 
     def read_input(self):
-        f = open('input.txt', 'r')
-        self.c = int(f.readline())
 
-        for x in range(0, self.c):
-            l = f.readline().strip().split(" ") # l=['2', 'mushrooms', 'tomatoes']
-            d = f.readline().strip().split(" ") # d=[1, 'Basil']
+        f = open('input/{}.txt'.format(self.filename), 'r')
+        cus = int(f.readline())
 
-            self.likes.update(l[1:])
-            self.dislikes.update(d[1:])
-
+        for _ in range(0, cus):
+            l = f.readline().strip().split(" ")
+            d = f.readline().strip().split(" ")
+            self.likes.update(list(l)[1:])
+            self.dislikes.update(list(d)[1:])
         f.close()
 
-    def solution(self):
-        self.read_input()
-        ingredients = []
-
-        for x in self.likes:
-            if self.likes[x] > self.dislikes[x]:
-                ingredients.append(x)
-
-        self.output(ingredients[:5])
-
-
-    def output(self, ingredients): # ingredients=['mushrooms', 'tomatoes']
-        f = open('output.txt', 'w')
+    def output(self, ingredients):
+        f = open('output/{}_output.txt'.format(self.filename), 'w')
         f.write(str(len(ingredients)))
         f.write(" ")
         f.write(" ".join(ingredients))
         f.close()
 
+
+    def top_5_ingredients(self):
+        self.read_input()
+
+        ingredients = []
+        for x in self.likes.most_common():
+            if self.likes[x[0]] > self.dislikes[x[0]]:
+                ingredients.append(x[0])
+        # ingredients.sort()
+        # ingredients = ingredients[:5]
+        self.output(ingredients)
+
+
 if __name__ == '__main__':
-    OnePizza().solution()
+    for x in ['a_an_example.in', 'b_basic.in', 'c_coarse.in', 'd_difficult.in', 'e_elaborate.in']:
+        OnePizza(x).top_5_ingredients()
